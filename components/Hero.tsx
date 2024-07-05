39ab5681ef6ce92c3c0f3da04b147c96223bf80d
tamayo2 <juan.tamayo3@utp.edu.co>
2024-07-05 17:55:47 -0500

    finalizando framer motion

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,6 +8,16 @@ const Hero = () => {
             <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill='purple'/>
             <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill='blue' />
         </div>
+
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+            <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
+        </div>
+
+        <div className="flex justify-center">
+            <div className='max-w-[89vw]'>
+                <h1></h1>
+            </div>
+        </div>
     </div>
   )
 }
