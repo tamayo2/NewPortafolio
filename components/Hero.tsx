01c26cfbad03dd81d1b999d3eedf8965df8e0007
tamayo2 <juan.tamayo3@utp.edu.co>
2024-07-06 14:42:51 -0500

    Finalice Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,7 @@
+import { FaLocationArrow } from 'react-icons/fa'
+import MagicButton from './ui/MagicButton'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
   return (
@@ -9,13 +12,32 @@ const Hero = () => {
             <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill='blue' />
         </div>
 
-        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
         </div>
 
-        <div className="flex justify-center">
-            <div className='max-w-[89vw]'>
-                <h1></h1>
+        <div className="flex justify-center relative my-20 z-10">
+            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+                <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+                My Portafolio: FullStack Developer
+                </h2>
+
+                <TextGenerateEffect 
+                    className='text-center text-[40px] md:text-5xl lg:text-6xl'
+                    words='Transforming Concepts into Seamless User Experiences'
+                />
+
+                <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+                    Hi, I&apos;m Sebastian, a FullStack Developer based in Colombia
+                </p>
+
+                <a href="#about">
+                    <MagicButton 
+                        title='Show my work'
+                        icon={<FaLocationArrow />}
+                        position='right'
+                    />
+                </a>
             </div>
         </div>
     </div>
