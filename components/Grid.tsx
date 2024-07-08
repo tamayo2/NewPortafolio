ae9751deb47a9c97829e31070d3c245be2a69915
tamayo2 <juan.tamayo3@utp.edu.co>
2024-07-07 22:40:24 -0500

    BentoGrid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
 
 const Grid = () => {
   return (
-    <div>Grid</div>
+    <section id='about'>
+      <BentoGrid >
+      {[{title: 'Title1', description: 'Desc1', id: 1}].map((item) => (
+        <BentoGridItem
+        id={item.id}
+        key={item.id}
+        title={item.title}
+        description={item.description}
+        />
+      ))}
+      </BentoGrid>
+    </section>
   )
 }
 
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -25,17 +25,19 @@ export const BentoGridItem = ({
   description,
   header,
   icon,
+  id,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
+  id: number;
 }) => {
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500",
         className
       )}
     >
