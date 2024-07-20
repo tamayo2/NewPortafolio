1bbd501500f38e8b60f3d6c80f2bbc391bede2ed
tamayo2 <juan.tamayo3@utp.edu.co>
2024-07-20 12:24:20 -0500

    Approach ajustes

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
-    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
