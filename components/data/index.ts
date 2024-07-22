28cedec609074b8fd1934fd1c99167c406dfc01b
tamayo2 <juan.tamayo3@utp.edu.co>
2024-07-22 16:10:04 -0500

    finish portafolio greaaat

--- components/data/index.ts
+++ components/data/index.ts
@@ -205,13 +205,16 @@ export const socialMedia = [
     {
       id: 1,
       img: "/git.svg",
+      link: "https://github.com/tamayo2",
     },
     {
       id: 2,
       img: "/twit.svg",
+      link: "https://github.com/tamayo2",
     },
     {
       id: 3,
       img: "/link.svg",
+      link: "https://github.com/tamayo2",
     },
 ];
